416e970ad0731e5653d54bdb5e4c89eb04cbfad7
Rithvik-29 <rithvik.rajesh@giis.edu.sg>
2021-07-30 17:00:02 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,11 +1,15 @@
+#This part initialises flask
 from flask import Flask,render_template
-
 app = Flask(__name__)
 
+#This variable is displayed on the homepage
 a = "It Works!"
+
+#This creates the home route because there is nothing after the '/'
 @app.route("/")
 def home():
     return render_template('home.html', a=a)
 
+#This part runs the server
 if __name__ == "__main__":
     app.run(debug=True, host="0.0.0.0")
