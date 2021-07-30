f815825f595cc8ca2480291ba6ef0ee2fb771196
Rithvik-29 <rithvik.rajesh@giis.edu.sg>
2021-07-30 16:52:36 +0800

    Added basic flask and jinja for testing
    Try running this code to make sure that your flask and python are working properly.

---
+++ main.py
@@ -0,0 +1,11 @@
+from flask import Flask,render_template
+
+app = Flask(__name__)
+
+a = "It Works!"
+@app.route("/")
+def home():
+    return render_template('home.html', a=a)
+
+if __name__ == "__main__":
+    app.run(debug=False, host="0.0.0.0")
